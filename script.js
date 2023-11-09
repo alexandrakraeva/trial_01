ffaa252bd4ea2674ad9c9c50faac2d92f9615184
alexandrakraeva <66311877+alexandrakraeva@users.noreply.github.com>
2023-11-09 13:55:45 +0100

    changes

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    const playButton = document.getElementById("play-button");
-    const animation = document.getElementById("animation");
+﻿const playButton = document.getElementById("play-button");
+const animation = document.getElementById("animation");
 
-    // Function to play the video
-    function playVideo() {
-        animation.style.display = "block";
-        animation.load(); // Load the video
-        animation.play(); // Play the video
-    }
+playButton.addEventListener("click", () => {
+    playButton.style.display = "none";
+    animation.style.display = "block";
+    animation.load(); // Load the video
+    animation.play(); // Play the video
+});
 
-    // Add a click event listener to the play button
-    playButton.addEventListener("click", playVideo);
-
-    // Add an ended event listener to the video element
-    animation.addEventListener("ended", function () {
-        animation.style.display = "none";
-        playButton.style.display = "block";
-    });
-
-    // On iPhones and iPads, trigger video playback on user interaction
-    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-    if (isIOS) {
-        playButton.style.display = "block"; // Show the play button
-        playButton.addEventListener("click", playVideo); // Play on button click
-    } else {
-        playVideo(); // Autoplay for other devices
-    }
+animation.addEventListener("ended", () => {
+    animation.style.display = "none";
+    playButton.style.display = "block";
 });
