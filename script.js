d3691e039744db9da491f02d1db7565488a7462a
alexandrakraeva <66311877+alexandrakraeva@users.noreply.github.com>
2023-11-09 13:40:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,28 @@
     const playButton = document.getElementById("play-button");
     const animation = document.getElementById("animation");
 
-    // Function to play the video and hide the play button
+    // Function to play the video
     function playVideo() {
-        playButton.style.display = "none";
         animation.style.display = "block";
         animation.load(); // Load the video
         animation.play(); // Play the video
-    }
-
-    // Function to show the play button when the video ends
-    function showPlayButton() {
-        animation.style.display = "none";
-        playButton.style.display = "block";
     }
 
     // Add a click event listener to the play button
     playButton.addEventListener("click", playVideo);
 
     // Add an ended event listener to the video element
-    animation.addEventListener("ended", showPlayButton);
+    animation.addEventListener("ended", function () {
+        animation.style.display = "none";
+        playButton.style.display = "block";
+    });
+
+    // On iPhones and iPads, trigger video playback on user interaction
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    if (isIOS) {
+        playButton.style.display = "block"; // Show the play button
+        playButton.addEventListener("click", playVideo); // Play on button click
+    } else {
+        playVideo(); // Autoplay for other devices
+    }
 });
